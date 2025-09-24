46cc8b5615adf29548a4022ea49d5040e4d969a1
MJ666 <michael.p.jakob@web.de>
2025-09-24 08:54:48 +0200

    HW4 Updates

--- board/project/sensor/esc_hw4.c
+++ board/project/sensor/esc_hw4.c
@@ -12,6 +12,9 @@
 #define TIMEOUT_US 2000
 #define PACKET_LENGHT 19
 #define SIGNATURE_LENGHT 13
+#define SIGNATURE_READ_COUNT 20
+#define INIT_DELAY 15000
+#define CELL_COUNT_DELAY 15000
 #define CURRENT_OFFSET_DELAY 15000
 #define NTC_BETA 3950.0
 #define NTC_R1 10000.0
@@ -20,12 +23,10 @@
 #define V_REF 3.3
 #define ADC_RES 4096.0
 
-float current_offset_ = -1;
-
-static void process(esc_hw4_parameters_t *parameter, int current_raw_offset, uint *current_raw);
-float get_voltage(uint16_t voltage_raw, esc_hw4_parameters_t *parameter);
+static void process(esc_hw4_parameters_t *parameter, int *current_raw_offset, uint *current_raw, float *voltage_scale, float *current_scale);
+float get_voltage(uint16_t voltage_raw, float v_scale);
 float get_temperature(uint16_t temperature_raw);
-float get_current(uint raw, int offset, float multiplier);
+float get_current(uint current_raw, int offset, float i_scale);
 
 void esc_hw4_task(void *parameters) {
     esc_hw4_parameters_t parameter = *(esc_hw4_parameters_t *)parameters;
@@ -48,21 +49,29 @@ void esc_hw4_task(void *parameters) {
     *parameter.cell_voltage = 3.75;
 #endif
 
-    if (parameter.init_delay) vTaskDelay(15000 / portTICK_PERIOD_MS);
+    if (parameter.init_delay) vTaskDelay(INIT_DELAY / portTICK_PERIOD_MS);
 
     TaskHandle_t task_handle;
-    uint cell_count_delay = 15000;
+    uint cell_count_delay = CELL_COUNT_DELAY;
     cell_count_parameters_t cell_count_parameters = {cell_count_delay, parameter.voltage, parameter.cell_count};
     xTaskCreate(cell_count_task, "cell_count_task", STACK_CELL_COUNT, (void *)&cell_count_parameters, 1, &task_handle);
     xQueueSendToBack(context.tasks_queue_handle, task_handle, 0);
 
     int current_raw_offset = -1;
     uint current_raw = 0;
+	float voltage_scale = 0.0;
+	float current_scale = 0.0;
+	if ((int)parameter.divisor) {
+        voltage_scale = V_REF / ADC_RES * parameter.divisor;
+    }
+    if ((int)parameter.current_multiplier) {
+        current_scale = V_REF / ADC_RES * parameter.current_multiplier;
+	}
     if (parameter.current_is_manual_offset) {
-        current_raw_offset = parameter.current_offset * 4096.0 / 3.3 / parameter.current_multiplier;
+        current_raw_offset = parameter.current_offset * ADC_RES / V_REF / parameter.current_multiplier;
         if (current_raw_offset > ADC_RES) current_raw_offset = ADC_RES;
     } else {
-        uint current_delay = 15000;
+        uint current_delay = CURRENT_OFFSET_DELAY;
         auto_offset_int_parameters_t current_offset_parameters = {current_delay, &current_raw, &current_raw_offset};
         xTaskCreate(auto_offset_int_task, "esc_hw4_current_offset_task", STACK_AUTO_OFFSET, &current_offset_parameters,
                     1, &task_handle);
@@ -73,14 +82,38 @@ void esc_hw4_task(void *parameters) {
 
     while (1) {
         ulTaskNotifyTakeIndexed(1, pdTRUE, portMAX_DELAY);
-        process(&parameter, current_raw_offset, &current_raw);
+        process(&parameter, &current_raw_offset, &current_raw, &voltage_scale, &current_scale);
     }
 }
 
-static void process(esc_hw4_parameters_t *parameter, int current_raw_offset, uint *current_raw) {
+static void process(esc_hw4_parameters_t *parameter, int *current_raw_offset, uint *current_raw, float *voltage_scale, float *current_scale) {
     uint16_t pwm, throttle;
     static uint32_t timestamp = 0;
+	static int signature_read_count = 0;
     uint8_t lenght = uart1_available();
+    if (lenght == SIGNATURE_LENGHT && signature_read_count < SIGNATURE_READ_COUNT) {
+        uint8_t data[SIGNATURE_LENGHT];
+        uart1_read_bytes(data, SIGNATURE_LENGHT);
+		if (data[1] == 0x9B && data[4] == 0x01 && data[12] == 0xB9) {
+			if (!(int)parameter->divisor && !(int)*voltage_scale) {
+                if (data[5] && data[6])
+                    *voltage_scale = (float)data[5] / (float)data[6] / 10;
+            }
+            if (!(int)parameter->current_multiplier && !(int)*current_scale) {
+                if (data[7] && data[8]) {
+                    *current_scale = (float)data[7] / (float)data[8];
+                    *current_raw_offset = (float)data[9] / *current_scale;
+                }
+            }
+            debug("\nEsc HW4 signature (%u) < SignatureReadCount: %i VScale: %f CRawOffset: %i IScale: %f\n",
+				uxTaskGetStackHighWaterMark(NULL), signature_read_count, *voltage_scale, *current_raw_offset, *current_scale);
+            debug_buffer(data, SIGNATURE_LENGHT, "0x%X ");
+			signature_read_count++;
+        } else {
+            debug("\nEsc HW4 signature packet error (%u): ", uxTaskGetStackHighWaterMark(NULL));
+            debug_buffer(data, SIGNATURE_LENGHT, "0x%X ");
+		}
+	}
     if (lenght == PACKET_LENGHT || lenght == PACKET_LENGHT + 1) {
         uint8_t data[PACKET_LENGHT];
         uart1_read_bytes(data, PACKET_LENGHT);
@@ -91,10 +124,10 @@ static void process(esc_hw4_parameters_t *parameter, int current_raw_offset, uin
         if (throttle < 1024 && pwm < 1024 && rpm < 200000 && data[11] <= 0xF && data[13] <= 0xF && data[15] <= 0xF &&
             data[17] <= 0xF) {
             *current_raw = (uint16_t)data[13] << 8 | data[14];
-            float voltage = get_voltage((uint16_t)data[11] << 8 | data[12], parameter);
+            float voltage = get_voltage((uint16_t)data[11] << 8 | data[12], *voltage_scale);
             float current = 0;
-            if (throttle > parameter->current_thresold / 100.0 * 1024 && current_raw_offset != -1) {
-                current = get_current(*current_raw, current_raw_offset, parameter->current_multiplier);
+            if (throttle > parameter->current_thresold / 100.0 * 1024 && *current_raw_offset != -1) {
+                current = get_current(*current_raw, *current_raw_offset, *current_scale);
                 if (current > parameter->current_max) current = parameter->current_max;
             }
             float temperature_fet = get_temperature((uint16_t)data[15] << 8 | data[16]);
@@ -102,7 +135,7 @@ static void process(esc_hw4_parameters_t *parameter, int current_raw_offset, uin
             rpm *= parameter->rpm_multiplier;
             if (parameter->pwm_out) xTaskNotifyGive(context.pwm_out_task_handle);
             *parameter->rpm = get_average(parameter->alpha_rpm, *parameter->rpm, rpm);
-            if (current_raw_offset != -1)
+            if (*current_raw_offset != -1)
                 *parameter->consumption += get_consumption(*parameter->current, parameter->current_max, &timestamp);
             *parameter->voltage = get_average(parameter->alpha_voltage, *parameter->voltage, voltage);
             *parameter->current = get_average(parameter->alpha_current, *parameter->current, current);
@@ -117,7 +150,7 @@ static void process(esc_hw4_parameters_t *parameter, int current_raw_offset, uin
                 "CellV: %.2f CRaw: %i CRawOffset: %i CurrMult: %.2f",
                 uxTaskGetStackHighWaterMark(NULL), packet, *parameter->rpm, *parameter->voltage, *parameter->current,
                 *parameter->temperature_fet, *parameter->temperature_bec, *parameter->consumption,
-                *parameter->cell_voltage, *current_raw, current_raw_offset, parameter->current_multiplier);
+                *parameter->cell_voltage, *current_raw, *current_raw_offset, parameter->current_multiplier);
         } else {
             debug("\nEsc HW4 packet error (%u): ", uxTaskGetStackHighWaterMark(NULL));
             debug_buffer(data, PACKET_LENGHT, "0x%X ");
@@ -125,8 +158,8 @@ static void process(esc_hw4_parameters_t *parameter, int current_raw_offset, uin
     }
 }
 
-float get_voltage(uint16_t voltage_raw, esc_hw4_parameters_t *parameter) {
-    return V_REF * voltage_raw / ADC_RES * parameter->divisor;
+float get_voltage(uint16_t voltage_raw, float v_scale) {
+    return voltage_raw * v_scale;
 }
 
 float get_temperature(uint16_t temperature_raw) {
@@ -138,9 +171,9 @@ float get_temperature(uint16_t temperature_raw) {
     return temperature;
 }
 
-float get_current(uint raw, int offset, float multiplier) {
+float get_current(uint current_raw, int offset, float i_scale) {
     // float current = (*parameter->current_raw - *parameter->current_offset) * V_REF / (parameter->ampgain *
     // DIFFAMP_SHUNT * ADC_RES);
-    if ((int)raw - offset < 0) return 0;
-    return ((int)raw - offset) * V_REF / ADC_RES * multiplier;
+    if ((int)current_raw - offset < 0) return 0;
+    return ((int)current_raw - offset) * i_scale;
 }
